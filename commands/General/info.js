8f066ee544763a0824e22b12412cdb4a6fa7e026
Jingexz01 <64733953+Jingexz01@users.noreply.github.com>
2021-06-23 19:14:36 +0800

    Update info.js

--- commands/General/info.js
+++ commands/General/info.js
@@ -28,7 +28,7 @@ module.exports = {
     )
     .setColor("RANDOM")
     .setDescription(`My global prefix is: \`${process.env.PREFIX}\`\n`)
-    .addField("<a:verified:857203241980264458> Developer", `${config.author} \[[Website](${config.authorwebsite})\]`)
+    .addField("<a:verified:852765188067164162> Developer", `${config.author} \[[Website](${config.authorwebsite})\]`)
     .setThumbnail(
      client.user.displayAvatarURL({
       dynamic: true,
@@ -42,11 +42,11 @@ module.exports = {
     .addField("⏳ Uptime", `${duration}`, true)
     .addField("🏓 Ping", Math.round(client.ws.ping) + "ms", true)
     .addField("💻Platform", osutils.platform(), true)
-    .addField("<a:node:857196447770214400> Node", `${process.version}`, true)
-    .addField("<a:cpu:857197771811323904> CPU Cores", osutils.cpuCount() + " Cores", true)
-    .addField("<a:ram:857197895338295317> Total Memory", osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split("")[0] + osutils.totalmem().toString().split(".")[1].split("")[1] + "MB", true)
-    .addField("<a:ram:857197895338295317> RAM Usage | Virtual Private Server (VPS) | 6 GB", `${(osutils.totalmem() - osutils.freemem()).toString().split(".")[0] + "." + (osutils.totalmem() - osutils.freemem()).toString().split(".")[1].split("")[0] + (osutils.totalmem() - osutils.freemem()).toString().split(".")[1].split("")[1]}/${osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split("")[0] + osutils.totalmem().toString().split(".")[1].split("")[1]}MB (${(100 - osutils.freememPercentage() * 100).toString().split(".")[0] + "." + (100 - osutils.freememPercentage() * 100).toString().split(".")[1].split("")[0] + (100 - osutils.freememPercentage() * 100).toString().split(".")[1].split("")[1]}%)`, true)
-    .addField("<a:ram:857197895338295317> RAM Usage (BOT) | 6 GB", (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB/" + osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split("")[0] + osutils.totalmem().toString().split(".")[1].split("")[1] + "MB", true)
+    .addField("<a:node:857196363196137472> Node", `${process.version}`, true)
+    .addField("<a:cpu:857197712763256832> CPU Cores", osutils.cpuCount() + " Cores", true)
+    .addField("<a:ram:857197346214117416> Total Memory", osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split("")[0] + osutils.totalmem().toString().split(".")[1].split("")[1] + "MB", true)
+    .addField("<a:ram:857197346214117416> RAM Usage | Virtual Private Server (VPS) | 6 GB", `${(osutils.totalmem() - osutils.freemem()).toString().split(".")[0] + "." + (osutils.totalmem() - osutils.freemem()).toString().split(".")[1].split("")[0] + (osutils.totalmem() - osutils.freemem()).toString().split(".")[1].split("")[1]}/${osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split("")[0] + osutils.totalmem().toString().split(".")[1].split("")[1]}MB (${(100 - osutils.freememPercentage() * 100).toString().split(".")[0] + "." + (100 - osutils.freememPercentage() * 100).toString().split(".")[1].split("")[0] + (100 - osutils.freememPercentage() * 100).toString().split(".")[1].split("")[1]}%)`, true)
+    .addField("<a:ram:857197346214117416> RAM Usage (BOT) | 6 GB", (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB/" + osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split("")[0] + osutils.totalmem().toString().split(".")[1].split("")[1] + "MB", true)
     .addField("Useful Links", `[Support server](${config.server}) | ${webpanel} [Invite me](https://discord.com/oauth2/authorize/?permissions=${config.premissions}&scope=bot&client_id=${client.user.id})`)
     .setFooter(
      "Requested by " + `${message.author.username}`,
@@ -62,7 +62,7 @@ module.exports = {
    message.lineReply({
     embed: {
      color: 16734039,
-     description: "Something went wrong, Please try again later.",
+     description: "Sorry something went wrong, Please try again later.",
     },
    });
   }
