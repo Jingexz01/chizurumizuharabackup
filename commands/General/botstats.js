7a54d12d8328e54149b6ec40d8e5edbdbe93f0f9
Jingexz01 <64733953+Jingexz01@users.noreply.github.com>
2021-06-25 10:34:33 +0800

    Update botstats.js

--- commands/General/botstats.js
+++ commands/General/botstats.js
@@ -15,7 +15,7 @@ const embed = new Discord.MessageEmbed()
 .setFooter("Chizuru Mizuhara")
 .setColor("GREEN")
 .addField("Bot Developer \| Bot Coder", "\`\`\`Jingexz01#2841\`\`\`")
-.addField("Official Bot Release Date", "\`\`\`November 11, 2020\`\`\`")
+.addField("Official Bot Release Date", "\`\`\`November 3, 2020\`\`\`")
 .addField("Bot Dashboard", `[Click Here](https://chizuru-mizuhara-database.herokuapp.com/dashboard)`)
 .addField("Bot Backup Dashboard\`\`\`(This will only work when bot is under backup launch or if you see the image below when opening the dashboard you should open either the main dashboard or the backup dashboard to see if what is working.)\`\`\`", `[Click Here](https://chizuru-mizuhara-databasebu.herokuapp.com)`)
 .setImage('https://media.discordapp.net/attachments/709959466623434804/857804543822790666/Screenshot_2021-06-25-10-02-21-279_com.android.chrome.png');
